79138f4eb45f1b0f40f3b834f142a51e357aedb5
Javed Naikawadi <javedn33@gmail.com>
2020-06-18 00:33:45 +0530

    added get mapping

--- src/main/java/com/picassotechnology/aws/awscodepipeline/HelloController.java
+++ src/main/java/com/picassotechnology/aws/awscodepipeline/HelloController.java
@@ -1,5 +1,6 @@
 package com.picassotechnology.aws.awscodepipeline;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	@GetMapping
 	public String hello() {
 		return "Hello YouTube!!!";
 	}
